513c0dcd64881f19c579904271a3d1a6b7ee7b86
DESKTOP-VSDKPQV\tmdgh <tmdghks7836@naver.com>
2021-12-28 12:49:06 +0900

    no message

--- src/main/java/class101/foo/io/Producer.java
+++ src/main/java/class101/foo/io/Producer.java
@@ -4,7 +4,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
 public class Producer {
 
     @Autowired
